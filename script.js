8abee2367607c706696f660587d0d82c7e50cfd3
WatchN0ne <bukuri6161@gmail.com>
2025-12-12 06:51:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+/* SCROLL REVEAL */
 const reveals = document.querySelectorAll(".reveal");
-
-function revealOnScroll() {
-  reveals.forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    if (top < window.innerHeight - 120) {
+function revealOnScroll(){
+  reveals.forEach(el=>{
+    if(el.getBoundingClientRect().top < innerHeight - 120){
       el.classList.add("show");
     }
   });
 }
-
-window.addEventListener("scroll", revealOnScroll);
+addEventListener("scroll", revealOnScroll);
 revealOnScroll();
+
+/* HERO MOUSE PARALLAX */
+const hero = document.querySelector(".hero");
+if(window.matchMedia("(hover:hover)").matches){
+  hero.addEventListener("mousemove", e=>{
+    const x=(e.clientX/innerWidth-.5)*30;
+    const y=(e.clientY/innerHeight-.5)*30;
+    hero.style.backgroundPosition=`calc(50% + ${x}px) calc(50% + ${y}px)`;
+  });
+}
